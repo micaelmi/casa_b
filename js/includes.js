507375271d52c26e17d5d6bbf7cfb4dbb6316bc1
garradefogo124 <micaelmiranda124@gmail.com>
2023-04-04 18:28:06 -0300

    Componentização de elementos e slider mobile

---
+++ js/includes.js
@@ -0,0 +1,18 @@
+fetch('./header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+
+fetch('./footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
+
+
+fetch('./whats.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('whats').innerHTML = data;
+    });
--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var hamburger = document.querySelector('.hamburger');
-    var menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
 
-    hamburger.addEventListener('click', function () {
-        var ativado = document.getElementById("hamburger").checked;
-        if (ativado) {
-            menu.classList.remove('active');
-            hamburger.classList.remove('active');
-        } else {
-            hamburger.classList.add('active');
-            menu.classList.add('active');
-        }
-    });
-});
+hamburger.onclick = function () {
+    const active = document.getElementById("hamburger").checked;
+    if (active) {
+        menu.classList.remove('active');
+        hamburger.classList.remove('active');
+    } else {
+        hamburger.classList.add('active');
+        menu.classList.add('active');
+    }
+}
