358a07861ca0a97ff1689d69166e9882ebfa3a9d
garradefogo124 <micaelmiranda124@gmail.com>
2023-05-18 19:17:11 -0300

    Otimização de imagens

--- js/slider-render.js
+++ js/slider-render.js
@@ -3,37 +3,37 @@ const carouselSmall = document.querySelector('.card-slider');
 
 let items = [
     {
-        image: 'img/material-basico-big.png',
+        image: 'img/material-basico-big.webp',
         alt: 'Caminhão de pedras',
         title: 'Material Básico',
         description: 'Contamos com os mais diversos materiais e insumos para fundações, estruturas e vedações, tais como tijolos, blocos, areia, brita, cimento, vergalhões e ferragens armadas, são muitas as opções para se estudar a princípio.',
     },
     {
-        image: 'img/impermeabilizacao-big.png',
+        image: 'img/impermeabilizacao-big.webp',
         alt: 'Caixas de impermeabilizantes',
         title: 'Impermeabilização',
         description: 'Iniciadas as obras, chega a hora de pensar em soluções que gerem durabilidade e estabilidade para a permanência dentro do sonhado ambiente, seja ele a casa perfeita, o local de trabalho acolhedor ou uma nova área de lazer. Nessa etapa, a impermeabilização é fundamental para que não haja infiltrações e evite transtornos futuros.',
     },
     {
-        image: 'img/hidraulica-eletrica-big.png',
+        image: 'img/hidraulica-eletrica-big.webp',
         alt: 'Canos e peças',
         title: 'Hidráulica e Elétrica',
         description: 'O planejamento correto de hidráulica e elétrica das construções é uma etapa importantíssima, afinal todos querem o conforto da iluminação adequada e tomadas disponíveis, vazão suficiente nas saídas de água além de banhos relaxantes.',
     },
     {
-        image: 'img/coberturas-big.png',
+        image: 'img/coberturas-big.webp',
         alt: 'Telhas',
         title: 'Coberturas',
         description: 'A cobertura da obra deve ser pensada ainda no projeto, visto que além de abrigo, os telhados geram muita carga nas fundações e são também acabamentos. Os materiais variam de acordo com as necessidades e estilo arquitetônico de cada cliente.',
     },
     {
-        image: 'img/acabamentos-big.png',
+        image: 'img/acabamentos-big.webp',
         alt: 'Latas de tinta',
         title: 'Acabamentos',
         description: 'A finalização! É hora de escolher tudo que realmente será visto nos ambientes, são tantos itens para se pensar: pisos, louças e metais sanitários; tintas e vernizes em suas infinitas aplicações e cores; acabamentos de elétrica, torneiras… temos muitas opções e você ainda pode contar com nossa equipe para tirar suas principais dúvidas quanto ao uso dos materiais!',
     },
     {
-        image: 'img/ferramentas-big.png',
+        image: 'img/ferramentas-big.webp',
         alt: 'Máquinas e ferramentas',
         title: 'Ferramentas',
         description: 'Você é um profissional da obra ou uma daquelas pessoas que adora fazer tudo sozinho e cria vários projetos?  A Casa B dispõe de uma linha muito atrativa para qualquer uma dessas hipóteses: ferramentas manuais e elétricas de diversas marcas e modelos além de peças para reposição como discos, lixas, bits, ponteiras, brocas, chaves e muito mais!',
